587c01ef55bbb63d3a737a997fc1f171115058a4
Sourav Das <132899011+Sdas08217@users.noreply.github.com>
2025-02-15 01:25:35 +0530

    Product of the Last K Numbers

---
+++ Feb_2025/Feb_14.py
@@ -0,0 +1,14 @@
+class ProductOfNumbers:
+    def __init__(self):
+        self.prefix_products = [1]
+    
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.prefix_products = [1]
+        else:
+            self.prefix_products.append(self.prefix_products[-1] * num)
+    
+    def getProduct(self, k: int) -> int:
+        if k >= len(self.prefix_products):
+            return 0
+        return self.prefix_products[-1] // self.prefix_products[-k - 1]
