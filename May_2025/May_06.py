4328df905e668937ed1a761be46bd870a16c3af1
Sourav Das <132899011+Sdas08217@users.noreply.github.com>
2025-05-05 23:56:03 -0400

    Build Array from Permutation

---
+++ May_2025/May_06.py
@@ -0,0 +1,6 @@
+# Build Array from Permutation
+from typing import List
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        return [nums[nums[i]] for i in range(len(nums))]
