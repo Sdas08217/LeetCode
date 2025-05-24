ad6962129bb8843a367ffbe13034710a2e0acd15
Sourav Das <132899011+Sdas08217@users.noreply.github.com>
2025-05-24 12:51:17 -0400

    Find Words Containing Character

---
+++ May_2025/May_24.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        return [i for i, word in enumerate(words) if x in word]
